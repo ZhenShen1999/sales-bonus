c3d98dd2012dbada9239095668c21ffbacf31aeb
ZhenShen <ZhenShen1999@yandex.ru>
2026-09-22 13:14:29 +0700

    Third iteration

--- src/main.js
+++ src/main.js
@@ -54,7 +54,7 @@ function analyzeSalesData(data, options) {
     // @TODO: Подготовка промежуточных данных для сбора статистики
     const sellerStats = data.sellers.map((seller) => ({
         seller_id: seller.id,
-        name: '${seller.first_name} ${seller.last_name}',
+        name: `${seller.first_name} ${seller.last_name}`,
         revenue: 0,
         profit: 0,
         sales_count: 0,
